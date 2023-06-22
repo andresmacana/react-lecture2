cccdccb331d8eccdadce8d68df71ab72e0c3e7b2
Jorge Macana <39782680+andresmacana@users.noreply.github.com>
2023-06-21 20:46:39 -0400

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Aside from "./Aside";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Main />
+      <Aside />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
