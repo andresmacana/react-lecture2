737075b9cbabafb067db1908ca26416c33e63e1d
Jorge Macana <39782680+andresmacana@users.noreply.github.com>
2023-06-22 11:28:59 -0400

    adding picture and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
+import { useState } from "react";
 import Header from "./header";
 import Nav from "./Nav";
 import Main from "./Main";
 import Aside from "./Aside";
 import Footer from "./Footer";
-import "./styles.css";
+import "./styles2.css";
+import Data from "./Data";
+import Hello from "./Hello";
+import myImage from "./hack1.jpg";
+
+const images = [
+  "https://picsum.photos/seed/picsum/200/300",
+  "https://picsum.photos/id/237/200/300",
+  "https://picsum.photos/id/102/200/300",
+  "https://picsum.photos/id/1025/200/300",
+];
 
 function App() {
+  const data = Data();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex(
+      (currentImageIndex - 1 + images.length) % images.length
+    );
+  };
+
+  /* const data = Data(); */
   return (
     <div>
       <Header />
       <Nav />
-      <Main />
-      <Aside />
+      <div className="main-content">
+        <div className="columns-container">
+          <Main />
+          <Aside />
+          <Hello name="Joe" />
+          <div className="carousel">
+            <button onClick={handlePrevImage}>Prev</button>
+            <img src={images[currentImageIndex]} alt="Carousel image" />
+            <button onClick={handleNextImage}>Next</button>
+          </div>
+        </div>
+        {/* <img src={images[currentImageIndex]} alt="Carousel image" />
+        <button onClick={handleNextImage}>Next</button> */}
+        {/* <img src={myImage} alt="coding" className="my-image" /> */}
+      </div>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <div className="columns">
+        <Main />
+        <Aside />
+        <Hello name="Joe" />
+      </div> */
+}
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,15 @@
 function Main() {
   return (
     <main>
-      <h1>Main</h1>
+      <h1>
+        <i className="bi bi-card-checklist"></i>Main
+      </h1>
+
+      <p>
+        Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce
+        dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+        fermentum massa justo sit amet.
+      </p>
     </main>
   );
 }
